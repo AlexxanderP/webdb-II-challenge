3363cf33725e262e1ec97d172f1bbe28784d468e
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-03-26 14:57:07 -0700

    configured knexJS library to connect to './data/lambda.sqlitre3'

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+
+const knex = require("knex"); // Step 1: Bring in the knexjs library
+
+const knexConfig = {
+  // Step 2: Configure the knexjs library to connect to
+  // './data/lambda.sqlite3' using sqlite3 module
+  client: "sqlite3", // this is the driver
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+  // debug: true
+};
 
 const server = express();
 
