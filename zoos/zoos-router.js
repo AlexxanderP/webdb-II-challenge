76d17720570a2c5a4ed3381b2281663007cc6381
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-03-26 15:35:21 -0700

    Get created inside zoos-router

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -12,4 +12,30 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
+//------ Get-----//
+
+router.get("/", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("zoos")
+    .where({ id })
+    .first()
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
